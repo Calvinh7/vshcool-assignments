6ec4676afac05d408ac88d24bf317f85e27943b4
Calvin Hardy <hardy.calvin2@gmail.com>
2017-11-21 09:35:23 -0700

    started local storage and the highscore game

---
+++ exercises/11.20-11.23/highscore/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import HighscoreForm from './components/HighscoreForm';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+
+export default App
---
+++ exercises/11.20-11.23/highscore/src/components/HighscoreForm.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class HighscoreForm extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+            gameTitle: "",
+            date: "",
+            highscore: "",
+            trashTalk: []
+        }
+    }
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default HighscoreForm
